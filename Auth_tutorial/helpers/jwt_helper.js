b4c6d8452dc07a5c3bfe2183fee9ba0bf79f185a
Madhur Saxena <saxenamadhur96@gmail.com>
2022-07-20 17:28:54 +0530

    added authentication using nodejs API

---
+++ Auth_tutorial/helpers/jwt_helper.js
@@ -0,0 +1,50 @@
+const JWT = require ('jsonwebtoken')
+const createError= require('http-errors')
+const { create } = require('../Models/User.model')
+
+
+module.exports={
+    signAccessToken:(userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = {  
+                
+            }
+            const secret=process.env.ACCESS_TOKEN_SECRET
+           
+            const options={
+                expiresIn: "1h",
+                issuer: "madhursaxena.co",
+                audience: userId,
+            }
+            JWT.sign(payload,secret,options,(err,token)=>{
+                if(err) {
+                    console.log(err.message)
+                    reject(createError.InternalServerError())
+                }
+
+                resolve(token)
+            })
+        })
+    },
+    verifyAccessToken: (req,res,next)=>{
+        if (!req.headers['authorization']) return next(createError.Unauthorized())
+        const authHeader=req.headers['authorization']
+        const bearerToken = authHeader.split(' ')
+        const token =bearerToken[1]
+        JWT.verify(token,process.env.ACCESS_TOKEN_SECRET,(err, payload)=>{
+            if(err){
+                // if(err.name==='JsonWebTokenError'){
+                //     return next(createError.Unauthorized())
+                // }else{
+                // return next(createError.Unauthorized(err.message))
+                // }
+                const message=err.name==='JsonWebTokenError' ? 'Unauthorized': err.meesage
+                return next(createError.Unauthorized(meesage))
+            }
+            req.payload = payload
+            next()
+        })
+    }
+}
+
+// module
